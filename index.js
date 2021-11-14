31f0b0371eb2c8b77a85d6eba051ec2f70aed2e1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com$ ssh-keygen -t rsa -C ruweydhaabdinoor@gmail.com>
2021-11-14 14:44:22 +0300

    Validated gender

--- index.js
+++ index.js
@@ -12,31 +12,36 @@ function validate(e){
   if (yearBorn.value == "" ){
     document.getElementById("year").style.border = "2px solid red";
     alert("Please fill out the year you were born");
- }
+  }
   else if (yearBorn.value.length !=4){
-    document.getElementById("year").style.border = "2px solid red";
+   document.getElementById("year").style.border = "2px solid red";
     alert("Enter a valid year")
     yearBorn.value ="";
- }
+  }
   else if (month.value == "" ){
-    document.getElementById("month").style.border = "2px solid red";
-    alert("Please fill out the month you were born");
-    month.value="";
-    
+   document.getElementById("month").style.border = "2px solid red";
+   alert("Please fill out the month you were born");
+   month.value="";    
   }
   else if (month.value <=0 || month.value > 12){
-    document.getElementById("month").style.border = "2px solid red";
-    alert("Enter a valid month");
-    month.value="";
- } 
+   document.getElementById("month").style.border = "2px solid red";
+   alert("Enter a valid month");
+   month.value="";
+  }
   else if (day.value == "" ){
     document.getElementById("day").style.border = "2px solid red"
     alert("Please fill out the day you were born");
     day.value="";
- }
+  }
   else if (day.value < 0 || day.value > 31){
     alert("Enter a valid day");
     document.getElementById("day").style.border = "2px solid red";
     day.value="";
+  }
+  else if (female.checked !=true && male.checked !=true  ){
+    alert("Please pick your gender");
+  }
+  else if (female.checked ==true && male.checked ==true  ){
+    alert("Please pick one gender");
   }
 }
