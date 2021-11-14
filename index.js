296481d97e97d18d0543bd41b48a17e76d75d969
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com$ ssh-keygen -t rsa -C ruweydhaabdinoor@gmail.com>
2021-11-14 14:18:08 +0300

    Created an array with Akan names for both genders

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var yearBorn = document.getElementById("year");
 var month = document.getElementById("month");
 var day = document.getElementById("day");
 var button = document.getElementById("btn");
+var femaleNames = ["Akosua", "Adwao", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
