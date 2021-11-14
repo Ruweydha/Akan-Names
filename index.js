8662be1a8a81738ec0e85bffc0fe6e8255dc65a1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com$ ssh-keygen -t rsa -C ruweydhaabdinoor@gmail.com>
2021-11-14 14:19:46 +0300

    Added an event listener to the button with a type of submit and a validate function

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var day = document.getElementById("day");
 var button = document.getElementById("btn");
 var femaleNames = ["Akosua", "Adwao", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+
+button.addEventListener("submit", validate);
