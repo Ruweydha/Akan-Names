a7220d75ef0108a66b385f6ac74a1f9c81ef2fc2
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com$ ssh-keygen -t rsa -C ruweydhaabdinoor@gmail.com>
2021-11-14 14:29:36 +0300

    Validated the year born input field

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ var femaleNames = ["Akosua", "Adwao", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
 
 button.addEventListener("submit", validate);
+
+function validate(e){
+  e.preventDefault();
+  if (yearBorn.value == "" ){
+      document.getElementById("year").style.border = "2px solid red";
+      alert("Please fill out the year you were born");
+  }
+  else if (yearBorn.value.length !=4){
+      document.getElementById("year").style.border = "2px solid red";
+      alert("Enter a valid year")
+      yearBorn.value ="";
+  }
+}
