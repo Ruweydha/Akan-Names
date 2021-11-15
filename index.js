cdce08c1e5ae3e7a5617d9a75a7359a88f6ecc04
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com$ ssh-keygen -t rsa -C ruweydhaabdinoor@gmail.com>
2021-11-15 11:06:51 +0300

    Added comments to the code and wrote a new function for generating akan name

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+//getting input from form by creating variables and arrays to be used.
 var yearBorn = document.getElementById("year");
 var month = document.getElementById("month");
 var day = document.getElementById("day");
 var button = document.getElementById("btn");
 var femaleNames = ["Akosua", "Adwao", "Abenaa","Akua", "Yaa", "Afua", "Ama"];
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi", "Kwame"];
+dayOfWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+//Adding event listener to button.
 button.addEventListener("submit", validate);
 
+//function for validating input from form
 function validate(e){
   e.preventDefault();
   if (yearBorn.value == "" ){
@@ -41,69 +45,33 @@ function validate(e){
   else if (female.checked !=true && male.checked !=true  ){
     alert("Please pick your gender");
   }
+  //if all input fields are validated, call on the function for generating akan name.
   else{
-    calculate();
+    generateName();
   }
 }
 
-function calculate(){
-  var century = yearBorn.value.slice(0,2);
-  var year = yearBorn.value.slice(2,4);
-  var cc = parseInt(century);
-  var yy = parseInt(year);
-  var mm = parseInt(month.value);
-  var dd = parseInt(day.value);
+//function for generating akan name
+function generateName(){
+ //creating a variable for the date
+  const date = `${month.value}-${day.value}-${yearBorn.value}`
+  //passing the value of the date variable
+  const d = new Date(date);
+  //creating a variable with the specific day someone was born.
+  let dayBorn = d.getDay();
+  
+//generating akan name for the males.
+  if(male.checked==true){
+    akanName = maleNames[dayBorn];
+    document.getElementById("akan").innerHTML = `You were born on a ${dayOfWeek[dayBorn]}. Your Akan name is ${maleNames[dayBorn]}`;
+    
+  }
+  //generating akan name for the females.
+  else {
+    akanName = femaleNames[dayBorn];
+    document.getElementById("akan").innerHTML = `You were born on a ${dayOfWeek[dayBorn]}. Your Akan name is ${femaleNames[dayBorn]}`;
 
-  if(cc<= 19){
-    var dayOfWeek = Math.floor(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7-1);
   }
-  else{
-    var dayOfWeek = Math.floor(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
-  }
-
-  if(female.checked ==true && dayOfWeek==0){
-      document.getElementById("akan").innerHTML = (`You were born on a Sunday. Your Akan name is ${femaleNames[0]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==1){
-    document.getElementById("akan").innerHTML = (`You were born on a Monday. Your Akan name is ${femaleNames[1]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==2){
-    document.getElementById("akan").innerHTML = (`You were born on a Tuesday. Your Akan name is ${femaleNames[2]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==3){
-    document.getElementById("akan").innerHTML = (`You were born on a Wednesday. Your Akan name is ${femaleNames[3]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==4){
-    document.getElementById("akan").innerHTML = (`You were born on a Thursday. Your Akan name is ${femaleNames[4]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==5){
-    document.getElementById("akan").innerHTML = (`You were born on a Friday. Your Akan name is ${femaleNames[5]}`);
-  }
-  else  if(female.checked ==true && dayOfWeek==6){
-    document.getElementById("akan").innerHTML = (`You were born on a Saturday. Your Akan name is ${femaleNames[6]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==0){
-    document.getElementById("akan").innerHTML = (`You were born on a Sunday. Your Akan name is ${maleNames[0]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==1){
-    document.getElementById("akan").innerHTML = (`You were born on a Monday. Your Akan name is ${maleNames[1]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==2){
-    document.getElementById("akan").innerHTML = (`You were born on a Tuesday. Your Akan name is ${maleNames[2]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==3){
-    document.getElementById("akan").innerHTML = (`You were born on a Wednesday. Your Akan name is ${maleNames[3]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==4){
-    document.getElementById("akan").innerHTML = (`You were born on a Thursday. Your Akan name is ${maleNames[4]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==5){
-    document.getElementById("akan").innerHTML = (`You were born on a Friday. Your Akan name is ${maleNames[5]}`);
-  }
-  else  if(male.checked ==true && dayOfWeek==6){
-    document.getElementById("akan").innerHTML = (`You were born on a Saturday. Your Akan name is ${maleNames[6]}`);
-  }
-
 }
 document.getElementById("btn").onclick = validate;
 document.getElementById("btn2").reset();
